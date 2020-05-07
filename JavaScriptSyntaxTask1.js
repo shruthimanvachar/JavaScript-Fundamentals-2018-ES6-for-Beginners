8809b47e77690aeafe8376d487d91dfcb365e560
shruthimanvachar <63493805+shruthimanvachar@users.noreply.github.com>
2020-05-07 12:15:28 +0530

    Update JavaScriptSyntaxTask1.js

--- JavaScriptSyntaxTask1.js
+++ JavaScriptSyntaxTask1.js
@@ -1,5 +1,9 @@
+/*
+
 Write the program using the following operators in JavaScript using JavaScript Editor
 a.	Arithmetic Operator
 b.	Comparison operator
 c.	Logical operator
 d.	Conditional Operator
+
+*/
