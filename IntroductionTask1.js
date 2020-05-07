0f79cb4f996ffc43c2d0b4a98578d08187e347fc
shruthimanvachar <63493805+shruthimanvachar@users.noreply.github.com>
2020-05-07 12:11:30 +0530

    Update IntroductionTask1.js

--- IntroductionTask1.js
+++ IntroductionTask1.js
@@ -1,1 +1,1 @@
-/* Download and setup the JavaScript Editor in the local machine and Write the code of the basic layout structure of the JavaScript using JavaScript Editor*/ 
+/* Write the code of the basic layout structure of the JavaScript using JavaScript Editor*/ 
