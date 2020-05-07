73351175a1a33df7e62701ed2fa44e7bfbee8f1c
shruthimanvachar <63493805+shruthimanvachar@users.noreply.github.com>
2020-05-07 12:20:07 +0530

    Create JavascriptSyntaxTask3.js

---
+++ JavascriptSyntaxTask3.js
@@ -0,0 +1,7 @@
+/*
+
+1. Write the program based on the JavaScript functions and default parameters in JavaScript using JavaScript Editor.
+
+2. Write the program based on the Arrow functions in JavaScript using JavaScript Editor.
+
+*/
